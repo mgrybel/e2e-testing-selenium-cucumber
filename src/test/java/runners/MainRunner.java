aa8fc5bfb85ec92e8a36a304f7dfce4953024f00
mgrybel <grybel.m.m@gmail.com>
2024-05-05 06:37:53 +0200

    Updated the MainRunner class

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -2,8 +2,14 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"}, tags = "@regression",
-        monochrome = true, dryRun = false, plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+        monochrome = true, dryRun = false, plugin = {"pretty", "html:target/cucumberReport.html", "json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
