0935c164b5a7d3907bf99f3d91579a0744ff6999
mgrybel <grybel.m.m@gmail.com>
2024-05-05 06:35:45 +0200

    Added the GlobalVars class

---
+++ src/main/java/utilities/GlobalVars.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class GlobalVars {
+    public static final int DEFAULT_EXPLICIT_TIMEOUT = 10;
+
+    public static final String ECOMMERCE_WEBSITE_URL = "http://localhost:3000";
+}
